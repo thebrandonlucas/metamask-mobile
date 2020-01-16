de117fffbab1808062ad86b42cb9225ce04deb8b
Bruno Barbieri <bruno.barbieri@consensys.net>
2020-01-15 21:24:13 -0500

    Revert "Improve onboarding (#1276)"
    This reverts commit c6cc8e6848b88572113e18d5f8710f682e8bff00.

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,15 +7,6 @@ MODE=$2
 TARGET=$3
 RUN_DEVICE=false
 PRE_RELEASE=false
-JS_ENV_FILE=".js.env"
-ANDROID_ENV_FILE=".android.env"
-IOS_ENV_FILE=".ios.env"
-
-envFileMissing() {
-	FILE="$1"
-	echo "'$FILE' is missing, you'll need to add it to the root of the project."
-	echo "For convenience you can rename '$FILE.example' and fill in the parameters."
-}
 
 displayHelp() {
     echo ''
@@ -106,9 +97,9 @@ prebuild(){
 	./node_modules/.bin/concat-cli -f app/core/InpageBridge.js node_modules/web3/dist/web3.min.js app/util/setProvider.js -o app/core/InpageBridgeWeb3.js
 	# Load JS specific env variables
 	if [ "$PRE_RELEASE" = false ] ; then
-		if [ -e $JS_ENV_FILE ]
+		if [ -e .js.env ]
 		then
-			source $JS_ENV_FILE
+			source .js.env
 		fi
 	fi
 }
@@ -133,9 +124,9 @@ prebuild_android(){
 	# Copy fonts with iconset
 	yes | cp -rf ./app/fonts/Metamask.ttf ./android/app/src/main/assets/fonts/Metamask.ttf
 	if [ "$PRE_RELEASE" = false ] ; then
-		if [ -e $ANDROID_ENV_FILE ]
+		if [ -e .android.env ]
 		then
-			source $ANDROID_ENV_FILE
+			source .android.env
 		fi
 	fi
 }
@@ -162,7 +153,7 @@ buildIosRelease(){
 	# Replace release.xcconfig with ENV vars
 	if [ "$PRE_RELEASE" = true ] ; then
 		echo "Setting up env vars...";
-		echo "$IOS_ENV" | tr "|" "\n" > $IOS_ENV_FILE
+		echo $IOS_ENV | tr "|" "\n" > .ios.env
 		echo "Build started..."
 		brew install watchman
 		cd ios && bundle install && bundle exec fastlane prerelease
@@ -170,7 +161,7 @@ buildIosRelease(){
 		yarn sourcemaps:ios
 	else
 		if [ ! -f "ios/release.xcconfig" ] ; then
-			echo "$IOS_ENV" | tr "|" "\n" > ios/release.xcconfig
+			echo $IOS_ENV | tr "|" "\n" > ios/release.xcconfig
 		fi
 		./node_modules/.bin/react-native run-ios  --configuration Release --simulator "iPhone 11 Pro (13.2)"
 	fi
@@ -186,7 +177,7 @@ buildAndroidRelease(){
 		TARGET="android/app/build.gradle"
 		sed -i'' -e 's/getPassword("mm","mm-upload-key")/"ANDROID_KEY"/' $TARGET;
 		sed -i'' -e "s/ANDROID_KEY/$ANDROID_KEY/" $TARGET;
-		echo "$ANDROID_KEYSTORE" | base64 --decode > android/keystores/release.keystore
+		echo $ANDROID_KEYSTORE | base64 --decode > android/keystores/release.keystore
 	fi
 
 	# GENERATE APK
@@ -212,31 +203,25 @@ checkParameters "$@"
 
 printTitle
 
-if [ "$PLATFORM" == "ios" ]; then
-	if [ -f "$IOS_ENV_FILE" ]; then
-		if [ "$MODE" == "release" ] ; then
-			buildIosRelease
-			else
-			if [ "$RUN_DEVICE" = true ] ; then
-				buildIosDevice
-			else
-				buildIosSimulator
-			fi
+if [ "$PLATFORM" == "ios" ]
+  	then
+
+	if [ "$MODE" == "release" ] ; then
+		buildIosRelease
+    else
+		if [ "$RUN_DEVICE" = true ] ; then
+			buildIosDevice
+		else
+			buildIosSimulator
 		fi
-	else
-		envFileMissing $IOS_ENV_FILE
 	fi
 
 
 
 else
-	if [ -f "$ANDROID_ENV_FILE" ]; then
-		if [ "$MODE" == "release" ] ; then
-			buildAndroidRelease
-		else
-			buildAndroid
-		fi
-	else
-		envFileMissing $ANDROID_ENV_FILE
+	if [ "$MODE" == "release" ] ; then
+		buildAndroidRelease
+    else
+		buildAndroid
 	fi
 fi
